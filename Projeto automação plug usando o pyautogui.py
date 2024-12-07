16283a0a04e44ce13ca2ffdd49bf37cd529d0cb2
GBJ008 <gabrielerik140@gmail.com>
2024-12-07 14:39:23 -0300

    Add files via upload
    Olá, nesse projeto eu tentei automatizar uma das funções do meu trabalho que é de fazer anúncios , mas eu usei o pyautogui, ou seja só vai funcionar na quele dispositivo ou um que tenha a mesma resolução, ele esta em faze de desenvolvimentos ou seja tem muita coisa para arrumar nele ainda , mas mesmo assim foi um bom começo para alguém que nunca usou essa ferramenta, aguardem novas atualizações  #GSJ

---
+++ Projeto automação plug usando o pyautogui.py
@@ -0,0 +1,176 @@
+import pyautogui as c
+import time 
+
+time.sleep(3)
+# press vai precionar a tecla que esteja dentro do parenteses e das aspas, nesse caso é a tecla win
+c.click(x=361, y=605)
+# ele vai clicar no no guadrado de todos os anúncios
+time.sleep(2)
+# aqui ele vai clicar em anúnciar 
+c.click(x=218, y=352)
+# anúnciar 
+time.sleep(2)
+c.click(x=1099, y=551)
+# selecionar conta
+time.sleep(3)
+c.click(x=847, y=349)
+# qual a conta 
+time.sleep(1)
+c.click(826, y=376)
+# selecionar qual é o tipo de anúncio que você quer 
+time.sleep(2)
+c.click(x=501, y=414)
+# anúncio class
+time.sleep(3)
+c.click(x=504, y=441)
+# clicar na descrição do anúncio
+time.sleep(1)
+c.click(x=579, y=539)
+# selecionar tudo
+c.hotkey('ctrl','a')
+# copiar a descrição
+c.hotkey('ctrl','c')
+# entrar no notpad
+c.hotkey('alt','tab')
+# cobia descrição
+c.hotkey('ctrl','z')
+# colar a descrição
+c.hotkey('ctrl','v')
+# copiar a descrição
+c.hotkey('ctrl','a')
+# copiar a descrição
+c.hotkey('ctrl','c')
+# sair do notpad
+c.hotkey('alt','tab')
+# clicar na descrição do anúncio
+time.sleep(1)
+c.click(x=579, y=539)
+# colar a descrição
+c.hotkey('ctrl','v')
+# publicar o anúncio
+c.click(x=348, y=165)
+# publicar o anúncio
+c.click(x=645, y=479)
+# PARTE 2============================================================================================================
+# selecionar conta
+time.sleep(3)
+c.click(x=847, y=349)
+# qual a conta 
+time.sleep(1)
+c.click(826, y=376)
+# selecionar qual é o tipo de anúncio que você quer 
+time.sleep(2)
+c.click(x=501, y=414)
+# anúncio class
+time.sleep(3)
+c.click(x=504, y=441)
+# clicar na descrição do anúncio
+time.sleep(1)
+c.click(x=579, y=539)
+# selecionar tudo
+c.hotkey('ctrl','a')
+# copiar a descrição
+c.hotkey('ctrl','c')
+# entrar no notpad
+c.hotkey('alt','tab')
+# cobia descrição
+c.hotkey('ctrl','z')
+# colar a descrição
+c.hotkey('ctrl','v')
+# copiar a descrição
+c.hotkey('ctrl','a')
+# copiar a descrição
+c.hotkey('ctrl','c')
+# sair do notpad
+c.hotkey('alt','tab')
+# clicar na descrição do anúncio
+time.sleep(1)
+c.click(x=579, y=539)
+# colar a descrição
+c.hotkey('ctrl','v')
+# publicar o anúncio
+c.click(x=348, y=165)
+# publicar o anúncio
+c.click(x=645, y=479)
+# PARTE 3============================================================================================================
+# selecionar conta
+time.sleep(3)
+c.click(x=847, y=349)
+# qual a conta 
+time.sleep(1)
+c.click(826, y=376)
+# selecionar qual é o tipo de anúncio que você quer 
+time.sleep(2)
+c.click(x=501, y=414)
+# anúncio class
+time.sleep(3)
+c.click(x=504, y=441)
+# clicar na descrição do anúncio
+time.sleep(1)
+c.click(x=579, y=539)
+# selecionar tudo
+c.hotkey('ctrl','a')
+# copiar a descrição
+c.hotkey('ctrl','c')
+# entrar no notpad
+c.hotkey('alt','tab')
+# cobia descrição
+c.hotkey('ctrl','z')
+# colar a descrição
+c.hotkey('ctrl','v')
+# copiar a descrição
+c.hotkey('ctrl','a')
+# copiar a descrição
+c.hotkey('ctrl','c')
+# sair do notpad
+c.hotkey('alt','tab')
+# clicar na descrição do anúncio
+time.sleep(1)
+c.click(x=579, y=539)
+# colar a descrição
+c.hotkey('ctrl','v')
+# publicar o anúncio
+c.click(x=348, y=165)
+# publicar o anúncio
+c.click(x=645, y=479)
+# PARTE 4============================================================================================================
+# selecionar conta
+time.sleep(3)
+c.click(x=847, y=349)
+# qual a conta 
+time.sleep(1)
+c.click(826, y=376)
+# selecionar qual é o tipo de anúncio que você quer 
+time.sleep(2)
+c.click(x=501, y=414)
+# anúncio class
+time.sleep(3)
+c.click(x=504, y=441)
+# clicar na descrição do anúncio
+time.sleep(1)
+c.click(x=579, y=539)
+# selecionar tudo
+c.hotkey('ctrl','a')
+# copiar a descrição
+c.hotkey('ctrl','c')
+# entrar no notpad
+c.hotkey('alt','tab')
+# cobia descrição
+c.hotkey('ctrl','z')
+# colar a descrição
+c.hotkey('ctrl','v')
+# copiar a descrição
+c.hotkey('ctrl','a')
+# copiar a descrição
+c.hotkey('ctrl','c')
+# sair do notpad
+c.hotkey('alt','tab')
+# clicar na descrição do anúncio
+time.sleep(1)
+c.click(x=579, y=539)
+# colar a descrição
+c.hotkey('ctrl','v')
+# publicar o anúncio
+c.click(x=348, y=165)
+# publicar o anúncio
+c.click(x=645, y=479)
